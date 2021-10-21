94c09d2f2a2cd4333cfa4d082706bb9502a381dc
Ronan <ronandowhite@gmail.com>
2021-10-21 10:29:36 +0100

    Notes and IndexOf solution
    Used indexof as the main solution to the count occurrences task. Notes about the solution and its speed / memory is in a separate text document.
    Other solutions like my initial regex solution are inside a commented region in the code

--- StringPerformanceTest/StringPerformanceTest/Program.cs
+++ StringPerformanceTest/StringPerformanceTest/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace StringPerformanceTest
 {
@@ -32,7 +33,58 @@ namespace StringPerformanceTest
         [Benchmark]
         public int CountOccurrences()
         {
-            
+            int totalCount = 0;
+            int currentIndex = 0;
+
+            for (int i = 0; i < NumberOfLines; i++)
+            {
+                // loops through all lines of the document
+                currentIndex = 0;
+
+                while ((currentIndex = Lines[i].IndexOf(SearchValue, currentIndex)) != -1) // find index of substring in line from previous index
+                {
+                    // found occurrence of substring in string
+                    totalCount += 1;
+                    currentIndex += 1; // increment so it doesnt find the same index next loop
+                }
+
+            }
+
+
+            return totalCount;
+
+            #region Other solutions
+            /// Other slower solutions I tried
+            /// Below uses regular expressions
+            /*
+
+            Regex regex = new Regex(SearchValue, RegexOptions.IgnoreCase);
+
+            for (int i = 0; i < NumberOfLines; i++)
+            {
+                totalCount += regex.Matches(Lines[i]).Count;
+            }
+            */
+
+            /// This method uses split
+            /*
+            for (int i = 0; i < NumberOfLines; i++)
+            {
+                // split returns an array 
+                totalCount += Lines[i].Split(new string[] { SearchValue }, StringSplitOptions.None).Count() - 1;
+            }
+            */
+
+            /// finds the word, and then removes it. compares original length of line with removed
+            /// version and uses that to count how many occurrences of the word was in the line
+            /*
+            for (int i = 0; i < NumberOfLines; i++)
+            {
+                totalCount += (Lines[i].Length - Lines[i].Replace(SearchValue, "").Length) / SearchValue.Length;
+            }
+            */
+
+            #endregion
         }
     }
 
